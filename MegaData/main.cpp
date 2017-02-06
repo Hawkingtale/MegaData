55af24fd7c0aa3b250ff2b18eab266267a70de87
Hawkingtale <mhac5527@740s-pl-124.local>
2017-02-06 12:22:06 -0700

    Initial Commit

---
+++ MegaData/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MegaData
+//
+//  Created by Hachtel, Matthew on 2/6/17.
+//  Copyright © 2017 Hachtel, Matthew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
