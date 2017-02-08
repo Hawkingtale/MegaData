a47c4082a5565ce3159a6112313240b6b54f095c
Hawkingtale <mhac5527@740s-pl-124.local>
2017-02-08 12:11:37 -0700

    IntNode is created and put in a few setters and getters

---
+++ MegaData/IntNode.hpp
@@ -0,0 +1,14 @@
+//
+//  IntNode.hpp
+//  MegaData
+//
+//  Created by Hachtel, Matthew on 2/8/17.
+//  Copyright © 2017 Hachtel, Matthew. All rights reserved.
+//
+
+#ifndef IntNode_hpp
+#define IntNode_hpp
+
+#include <stdio.h>
+
+#endif /* IntNode_hpp */
