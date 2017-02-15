e37a7cb4e38cd8efa1acd88cfce6d3705bf08d4f
Hawkingtale <mhac5527@740s-pl-124.local>
2017-02-15 13:02:14 -0700

    updated Array template with a header

--- MegaData/Array.h
+++ MegaData/Array.h
@@ -26,4 +26,65 @@ public:
     int getSize();
 };
 
+template <class Type>
+Array<Type> :: Array()
+{
+    //Never used
+    // Default construtor only supplied to avoid complication error!
+}
+
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0)
+    
+    this->size = size;
+    this->front = new Node<Type>();
+    
+    for(int index = 1; index < size; index++)
+    {
+        Node<Type> * current = new Node<Type>();
+        current->setNodePointer(front);
+        front = current;
+    }
+}
+
+template <class Type>
+viud Array<Type> :: setAtIndex(int index, Type data)
+{
+    assert(index >= 0 && index < size);
+    Node<Type>  * current = front;
+    for(int spot =0; spot < index; spot++)
+    {
+        current = current->getNodePointer();
+    }
+    
+    value = current->getNodePointer();
+    
+    return value;
+}
+
+template <class Type>
+Type array<Type> :: getFromIndex(int index)
+{
+    assert(index > 0 && index < size);
+    
+    Node<Type>* current = front;
+    for(int index = 0; postition < index; position++)
+    {
+        current = current->getNodePointer();
+    }
+    
+    value = current->getNodeData();
+    
+    return value;
+}
+
+template <class Type>
+int Array<Type> :: getSize()
+{
+    return size;
+}
+
+
 #endif /* Array_h */
